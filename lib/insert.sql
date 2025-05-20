1909e992bb1dc76cc0cdc21022d57539d859ba8a
tinamanga <tinamanga@gmail.com>
2025-05-20 20:47:21 +0300

    Use SQL to store data and retrieve it later on. Use SQLite to build relational databases on your computer. Perform CRUD operations on relational databases using SQL.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 7, 'M', 'brown', 'calm', 1),
+('Rowdy', 4, 'M', 'black', 'aggressive', 1),
+('Tabitha', 6, 'F', 'gray', 'friendly', 1),
+('Sergeant Brown', 9, 'M', 'dark brown', 'serious', 0),
+('Melissa', 5, 'F', 'blonde', 'gentle', 1),
+('Grinch', 8, 'M', 'green', 'grumpy', 0),
+('Wendy', 3, 'F', 'white', 'playful', 1),
+(NULL, 2, 'F', 'black', 'mysterious', 1)
